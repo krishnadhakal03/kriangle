017cc9c34cbad5f05cf6c2bdc879a181600d1fbe
krishnadhakal03 <krishna.dhakal03@gmail.com>
2025-03-16 17:19:26 -0400

    Update wsgi.py
    wsgi changes

--- kriangle/wsgi.py
+++ kriangle/wsgi.py
@@ -1,21 +1,14 @@
-"""
-WSGI config for kriangle project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 import sys
+# Path to your virtual environment
+venv_path = '/home/ubuntu/kriangle/venv'
 
-# Add virtual environment to sys.path
-activate_this = '/home/ubuntu/kriangle/venv/bin/activate_this.py'
-exec(open(activate_this).read(), {'__file__': activate_this})
+# Add the virtual environment's site-packages to sys.path
+sys.path.insert(0, os.path.join(venv_path, 'lib', 'python3.12', 'site-packages'))
 
-from django.core.wsgi import get_wsgi_application
-
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kriangle.settings')
 
+# Import the application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
